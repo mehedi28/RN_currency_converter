f4b3a7925e0b819dedcaf63827e1b9b57e7196b7
mehedi28 <mehedi.hasan28.bd@gmail.com>
2020-05-08 19:59:42 +0600

    added react navigation/native and react navigation/stack packages

--- CurrencyConv/App.js
+++ CurrencyConv/App.js
@@ -1,10 +1,21 @@
+
 import React, {Component} from 'react';
 import Home from './app/screens/Home';
 import CurrencyList from './app/screens/CurrencyList';
 
+import { NavigationContainer } from '@react-navigation/native';
+//import { createStackNavigator } from '@react-navigation/stack';
+
+//const Stack = createStackNavigator();
+
 class App extends Component {
     render() {
         return (
+            /*<NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="CurrencyList" component={CurrencyList} />
+                </Stack.Navigator>
+            </NavigationContainer>*/
             <CurrencyList/>
         );
     }
